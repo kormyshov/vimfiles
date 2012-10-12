a6f541eccc31cd073d4c1a6628e7863e36365ea1
Mikhail Kormyshov <kormyshov@gmail.com>
2012-10-12 16:55:30 +0800

    Modify cpp4cf plugin

---
+++ cpp4cf_template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+
+typedef LL long long
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
+
+// [[MAIN]]
+
+// [[TESTS]]
