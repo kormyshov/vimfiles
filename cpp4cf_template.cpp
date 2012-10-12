fab44d992e7c0aef2cf66569b8b85ad05bed0f1c
Mikhail Kormyshov <kormyshov@gmail.com>
2012-10-12 17:12:47 +0800

    Modify template and add snippet test

--- cpp4cf_template.cpp
+++ cpp4cf_template.cpp
@@ -2,8 +2,34 @@
 #include <fstream>
 #include <cstdio>
 #include <cstring>
+#include <memory.h>
+#include <cmath>
 
-typedef LL long long
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+#define ABS(a) ((a)<0?(-(a)):(a))
+#define SIGN(a) ((a)<0?(-1):((a)>0?(1):(0)))
+#define SQR(a) ((a)*(a))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+
+#define PI (3.1415926)
+#define INF (2147483647)
+#define INF2 (1073741823)
+#define EPS (0.00000001)
+
+#define MOD (1000000007)
+
+#define y1 stupid_cmath
+#define y0 stupid_cmath_too
+
+typedef long long LL
 
 using namespace std;
 
